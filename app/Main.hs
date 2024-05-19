ed6aea33f3dbe9be82b942bcc265c4246e99ffb9
Katarzyna Mielnik <kasia.a.mielnik@gmail.com>
2024-05-19 20:07:12 +0200

    modify main

--- app/Main.hs
+++ app/Main.hs
@@ -3,32 +3,11 @@ module Main where
 import Interpreter
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
-import Prelude
-  ( IO,
-    putStrLn,
-    unlines,
-    ($),
-  )
+import Prelude (IO, putStrLn, (>>))
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    ["--help"] -> usage
     [f] -> interpretFile f
-    _ -> invalidUsage
-
-usage :: IO ()
-usage = do
-  putStrLn $
-    unlines
-      [ "usage: Call with one of the following argument combinations:",
-        "  --help          Display this help message.",
-        "  (file)         Parse content of the file."
-      ]
-  exitFailure
-
-invalidUsage :: IO ()
-invalidUsage = do
-  putStrLn "Invalid usage! Use --help."
-  exitFailure
+    _ -> putStrLn "Invalid usage!" >> exitFailure
